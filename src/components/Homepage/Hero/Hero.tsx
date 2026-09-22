82df4b9f584fff5fbb7399e4fb6ee6d912654635
rare-aniket <aniket@rarepixelsdesign.com>
2026-09-22 14:08:09 +0530

    RarePixels - Reborn Initial Project Added

---
+++ src/components/Homepage/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+import "./hero.css"
+const Hero = () => {
+    return (
+        <section className="section home-hero bg-[#ED0180] text-white h-[100vh]">
+            <div className="container h-[100%]">
+                <div className="h-[100%] flex flex-col items-center justify-center gap-[20px]">
+                    <h1>Jay Shree Ganesh</h1>
+
+                    <h2>Welcome to RarePixels Reborn</h2>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Hero
