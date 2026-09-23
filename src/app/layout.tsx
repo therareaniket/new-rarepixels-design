8913de96c98246680ab05eb9327afd4edef2485a
rare-aniket <aniket@rarepixelsdesign.com>
2026-09-23 20:41:43 +0530

    fix 🐛: Testimonials Mobile Slider Not working Fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,10 @@ const strichpunktSans = Strichpunkt_Sans({
 export const metadata: Metadata = {
 	title: "RarePixels Design | Thinking of an Idea is Easy but Make them Happen is Rare",
 	description: "Unleash the full potential of your brand with RarePixels Design LLP, a creative powerhouse offering unparalleled UI/UX design, web and mobile app development, and branding solutions. Our skilled team of digital enthusiasts has consistently elevated businesses of all sizes. With an unmatched ability to envision and construct engaging user experiences, we bring your digital vision to life confidently, propelling your brand to new heights.",
+	robots: {
+		index: false,
+		follow: false,
+	},
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
@@ -23,7 +27,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
 			<body className="min-h-full flex flex-col">
 				<Header />
 
-				{children}
+				<main>
+					{children}
+				</main>
 				
 				<Footer />
 			</body>
